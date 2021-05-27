ec1902f231ea485802e2ebc361b2aed103ccf5dc
Binatik <sadxas999@mail.ru>
2021-05-27 23:56:27 +0300

    добавлен  store и улучшена шапка  - header

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,28 @@
-const swiper = new Swiper('.swiper-container', {
-    // Optional parameters
-    loop: true,
+const swiperW = document.querySelector('.swiper-wrapper');
+const pagination = document.querySelector('.pagination__list');
 
-    // If we need pagination
+images.map(({url, alt}) => {
+    swiperW.innerHTML +=
+        ` 
+        <div class="swiper-slide">
+           <img class="swiper-slide__img" src=${url} alt=${alt}>
+        </div> 
+        `
+});
+
+pagination.map(({link, title}) => {
+    swiperW.innerHTML +=
+        ` 
+        <li class="pagination__item">
+            <a class="pagination__link link" href=${link}>${title}</a>
+        </li>
+        `
+});
+
+const swiper = new Swiper('.swiper-container', {
+    loop: true,
+    spaceBetween: 15,
     pagination: {
         el: '.swiper-pagination',
-    },
-
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-
-    // And if we need scrollbar
-    scrollbar: {
-        el: '.swiper-scrollbar',
     },
 });
---
+++ store.js
@@ -0,0 +1,33 @@
+const images = [
+    {
+        url: 'images/mainSlider1.jpg',
+        alt: 'свадьба'
+    },
+    {
+        url: 'images/mainSlider1.jpg',
+        alt: 'свадьба'
+    },
+]
+
+const a = [
+    {
+        link: '#',
+        title: 'Главная'
+    },
+    {
+        link: '#',
+        title: 'Обо мне'
+    },
+    {
+        link: '#',
+        title: 'Услуги'
+    },
+    {
+        link: '#',
+        title: 'Портфолио'
+    },
+    {
+        link: '#',
+        title: 'Контакты'
+    },
+]
