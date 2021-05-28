b249c9b37c923b5043dd234a8dbd27556c2b141b
Binatik <sadxas999@mail.ru>
2021-05-28 22:36:59 +0300

    add person.js

---
+++ scripts/person.js
@@ -0,0 +1,12 @@
+const person = [
+    {
+        id: 'Аделина',
+        email: 'JoseVilla@gmail.com',
+        tel: '+7903 771 45 46',
+        link: {
+            twitter: '#',
+            instagram: '#',
+            test: '#'
+        }
+    }
+]
